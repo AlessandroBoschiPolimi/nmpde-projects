7c15a88df544178e3a62b0098954e7caeff82823
Gab-San <santandreagabriele+github@gmail.com>
2025-12-09 14:36:32 +0100

    Properly rewritten bases functions

--- src/NeoHooke.cpp
+++ src/NeoHooke.cpp
@@ -19,6 +19,8 @@
 #include <deal.II/numerics/vector_tools.h>
 #include <deal.II/numerics/matrix_tools.h>
 
+#define GAMBA_DEBUG true
+
 using namespace pde;
 
 void NeoHooke::setup() {
@@ -48,7 +50,7 @@ void NeoHooke::setup() {
 	Triangulation<dim> mesh_serial;
 	std::cout << "Initializing the mesh" << std::endl;
 	GridGenerator::subdivided_hyper_cube(mesh, num_cells, 0.0, 1.0, true);
-	std::cout << "  Number of elements = " << mesh.n_active_cells()
+	std::cout << "Number of elements = " << mesh.n_active_cells()
 		  << std::endl;
     }
 
@@ -56,13 +58,16 @@ void NeoHooke::setup() {
 
     // Initialize the finite element space
     {
+	// TODO: Check that this Finite Element Space suffices
 	fe = std::make_unique<FESystem<dim>>(FE_Q<dim>(r)^dim);
 
 	std::cout << "  Degree                     = " << fe->degree << std::endl;
 	std::cout << "  DoFs per cell              = " << fe->dofs_per_cell
 		  << std::endl;
 
+	// TODO: Check that these quadrature are correct enough
 	quadrature = std::make_unique<QGaussSimplex<dim>>(r + 1);
+	quadrature_boundary = std::make_unique<QGaussSimplex<dim - 1>>(r + 1);
 
 	std::cout << "  Quadrature points per cell = " << quadrature->size()
 	  << std::endl;
@@ -98,11 +103,11 @@ void NeoHooke::setup() {
 
 	// Then, we use the sparsity pattern to initialize the system matrix
 	std::cout << "  Initializing the system matrix" << std::endl;
-	system_matrix.reinit(sparsity_pattern);
+	jacobian_matrix.reinit(sparsity_pattern);
 
 	// Finally, we initialize the right-hand side and solution vectors.
 	std::cout << "  Initializing the system right-hand side" << std::endl;
-	system_rhs.reinit(dof_handler.n_dofs());
+	residual_vector.reinit(dof_handler.n_dofs());
 	std::cout << "  Initializing the solution vector" << std::endl;
 	solution.reinit(dof_handler.n_dofs());
 	delta.reinit(dof_handler.n_dofs());
@@ -121,20 +126,35 @@ void NeoHooke::assemble_system() {
 	update_quadrature_points | update_JxW_values
     );
 
+    FEFaceValues<dim> fe_values_boundary(
+	*fe,
+	*quadrature_boundary,
+	update_values | update_quadrature_points |
+	update_JxW_values
+    );
+
+
     FullMatrix<double>	cell_matrix(dofs_per_cell, dofs_per_cell);
     Vector<double>	cell_rhs(dofs_per_cell);
 
     std::vector<types::global_cell_index> dof_indices(dofs_per_cell);
 
-    system_matrix = 0.0;
-    system_rhs 	  = 0.0;
+    jacobian_matrix = 0.0;
+    residual_vector 	  = 0.0;
 
     std::vector<Tensor<1,dim, double>>	solution_loc(n_q);
     std::vector<Tensor<2,dim, double>>	solution_gradient_loc(n_q);
+    
 
-    // Getting a view of the <2,dim> tensor from 0:dim-1
+    // Getting a view of the <1,dim> tensor from 0:dim-1
+    // An FEValuesExtractor defines a view of the vector
+    // (in this case the view contains the exact values we are using
+    // due to only needing displacement)
+    // on which then the FEValues can perform operations such as
+    // in this case to compute gradients of vectors of vectors
     const FEValuesExtractors::Vector displacement(0);
-    double value;
+
+
     for ( const auto &cell : dof_handler.active_cell_iterators() ) {
 
 	fe_values.reinit(cell);
@@ -144,48 +164,93 @@ void NeoHooke::assemble_system() {
 	fe_values[displacement].get_function_values(solution, solution_loc);
 	fe_values[displacement].get_function_gradients(solution, solution_gradient_loc);
 
-	for ( const unsigned int i : fe_values.dof_indices() ) {
-	    const unsigned int comp_i =
-		fe->system_to_component_index(i).first;
-	    for ( const unsigned int q : fe_values.quadrature_point_indices() ) {
+	#if GAMBA_DEBUG
+	for ( const unsigned int q : fe_values.quadrature_point_indices() ) {
+		std::cout << "fe_values[displacement].get_function_values " << 
+		    solution_loc[q] << std::endl;
+		std::cout << "fe_values[displacement].get_function_gradients " << 
+		   solution_gradient_loc[q] << std::endl;
+	}
+	#endif
+
+	for ( const unsigned int q : fe_values.quadrature_point_indices() ) {
+	    for ( const unsigned int i : fe_values.dof_indices() ) {
+		// base to describe v
+		const Tensor<2,dim> phi_i_grad = fe_values[displacement].gradient(i, q);
+
 		Tensor<2,dim> displacement_tensor({{1,0,0},{0,1,0},{0,0,1}});
 		displacement_tensor += solution_gradient_loc[q];
-		Tensor<2,dim> inverse_displacement = transpose(invert(displacement_tensor));
+		const Tensor<2,dim> inverse_displacement = transpose(invert(displacement_tensor));
+
 		for ( const unsigned j : fe_values.dof_indices() ) {
-		    const unsigned int comp_j =
-			fe->system_to_component_index(j).first;
-		    value = 0;
-		    for(unsigned int a = 0; a < dim; a++)
-			value += inverse_displacement[a][comp_j] * fe_values.shape_grad(i, q)[comp_j] * inverse_displacement[comp_i][a];
-		   
+		    // base to describe delta
+		    const Tensor<2, dim> phi_j_grad = fe_values[displacement].gradient(j,q);
+		    const Tensor<2,dim> second_member = inverse_displacement * transpose(phi_j_grad) * inverse_displacement;
 
-		    cell_matrix(i,j) += comp_i == comp_j
-			? fe_values.shape_grad(i,q) * fe_values.shape_grad(j,q)
-			* fe_values.JxW(q) : 0;
+		    // TODO: check correctness of the following multiplication
+		    // and if there is an overloaded operator to do it
+		    cell_matrix(i,j) += mu * (
+			double_contract<0,0,1,1>(phi_j_grad, phi_i_grad) + 
+			double_contract<0,0,1,1>(second_member , phi_i_grad)
+			) * fe_values.JxW(q);
+		    
 
-		    cell_matrix(i,j) += value * fe_values.shape_grad(j,q)[comp_i] * fe_values.JxW(q);
 		}
 
-		for(unsigned int j = 0; j < dim; j++) {
-		    // TODO: Check this computation cause I'm not conviced it is right in any way
-		    cell_rhs(i) -= displacement_tensor[j][comp_i] * fe_values.shape_grad(i,q)[comp_i] * fe_values.JxW(q);
-		    cell_rhs(i) -= inverse_displacement[j][comp_i] * fe_values.shape_grad(i,q)[comp_i] * fe_values.JxW(q);
-		}
-
+		cell_rhs(i) -= mu * (
+		    double_contract<0,0,1,1>(displacement_tensor, phi_i_grad) -
+		    double_contract<0,0,1,1>(inverse_displacement, phi_i_grad)
+		) * fe_values.JxW(q);
 	    }
 	}
 	
+	if(cell->at_boundary()) {
+	    for (unsigned int face_number = 0; 
+		face_number < cell->n_faces();
+		face_number++) {
+		    // If current face lies on the boundary, and its boundary ID (or
+		    // tag) is that of one of the Neumann boundaries, we assemble the
+		    // boundary integral.
+		    if ( cell->face(face_number)->at_boundary() &&
+			( cell->face(face_number)->boundary_id() == 4 ||
+			cell->face(face_number)->boundary_id() == 5 )
+		    ) {
+		    fe_values_boundary.reinit(cell, face_number);
+
+		    for (unsigned int q : fe_values_boundary.quadrature_point_indices()) {
+			for (unsigned int i = 0; i < dofs_per_cell; ++i) {
+
+			    #if GAMBA_DEBUG
+				std::cout << "Point(q) " << fe_values_boundary.quadrature_point(q)  << std::endl;
+				std::cout << "fe_values_boundary[displacement].value(i, q) " << fe_values_boundary[displacement].value(i, q) << std::endl;
+			    #endif
+
+			    cell_rhs(i) +=
+			    h(fe_values_boundary.quadrature_point(q)) *
+			    fe_values_boundary[displacement].value(i, q) *     
+			    fe_values_boundary.JxW(q);
+		    	}
+		    }
+		}
+	    }
+	}
+ 
 	// At this point the local matrix and vector are constructed: we need
 	// to sum them into the global matrix and vector. To this end, we need
 	// to retrieve the global indices of the DoFs of current cell.
 	cell->get_dof_indices(dof_indices);
 
-	system_matrix.add(dof_indices, cell_matrix);
-	system_rhs.add(dof_indices, cell_rhs);
+	jacobian_matrix.add(dof_indices, cell_matrix);
+	residual_vector.add(dof_indices, cell_rhs);
     }
+    
+    #if GAMBA_DEBUG
+	std::cout << "system_rhs " << residual_vector << std::endl;
+    #endif
 
-    // TODO: add boundary conditions
-	//    {
+    // TODO: add dirichelet boundary conditions
+
+    //    {
 	// std::map<types::global_dof_index, double> boundary_values;
 	//
 	// std::map<types::boundary_id, const Function<dim> *> boundary_functions;
@@ -204,14 +269,15 @@ void NeoHooke::assemble_system() {
 }
 
 void NeoHooke::solve_system() {
-    SolverControl solver_control(1000, 1e-6 * system_rhs.l2_norm());
+    // TODO: Change number of iterations
+    SolverControl solver_control(2000, 1e-6 * residual_vector.l2_norm());
 
     SolverGMRES<Vector<double>> solver(solver_control);
     
     PreconditionSSOR preconditioner;
-    preconditioner.initialize(system_matrix);
+    preconditioner.initialize(jacobian_matrix);
 
-    solver.solve(system_matrix, delta, system_rhs, preconditioner);
+    solver.solve(jacobian_matrix, delta, residual_vector, preconditioner);
     std::cout << "   " << solver_control.last_step() << " GMRES iterations"
     << std::endl;
 
@@ -229,7 +295,7 @@ void NeoHooke::solve() {
     while (n_iter < n_max_iters && residual_norm > residual_tolerance)
     {
 	assemble_system();
-	residual_norm = system_rhs.l2_norm();
+	residual_norm = residual_vector.l2_norm();
 
 	std::cout << "Newton iteration " << n_iter << "/" << n_max_iters
 	<< " - ||r|| = " << std::scientific << std::setprecision(6)
@@ -239,7 +305,7 @@ void NeoHooke::solve() {
 	// tolerance.
 	if (residual_norm <= residual_tolerance) break;
 
-        solve_system();
+	solve_system();
 	solution += delta;
 
 	++n_iter;
