97362eb72b9e6768fc12ed1f237e66c33943fb5d
Gab-San <santandreagabriele+github@gmail.com>
2025-12-09 00:54:39 +0000

    Rewritten the whole system

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
+#include <NeoHooke.hpp>
+
 int main(int argc, char *argv[])
 {
-	return 0;
+    using namespace pde;
+    const unsigned int dim = NeoHooke::dim;
+    const unsigned int r = 1;
+    const unsigned int num_cells = 2;
+    std::function<Point<dim> (const Point<dim> &)> h;
+    NeoHooke problem = NeoHooke("", r, h, num_cells);
+    problem.setup();
+    problem.solve();
+    return 0;
 }
